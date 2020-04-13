1b717df4b698cfedf842cd043c994198beacedb8
benwk <z@benwk.com>
2020-04-13 23:03:22 +0800

    修复webpack安装时title undefined的问题

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = function(e, n) {
   "function" == typeof define && (define.amd || define.cmd) ? define(function() {
     return n(e)
   }) : n(e, !0)
-} (this,
+} (window,
   function(o, e) {
     if (!o.jWeixin) {
       var n, c = {

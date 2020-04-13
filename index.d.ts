03a2859a2cd7830ac1a00f8dafecf4eb722019ad
benwk <z@benwk.com>
2020-04-13 22:45:15 +0800

    更新TS版本

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+/**
+ * WeChat js-sdk modified for npm
+ *
+ * @author benwk
+ * @date 20200413
+ *
+ * Wechat js file: https://res.wx.qq.com/open/js/jweixin-1.6.0.js
+ */
+
+declare module "wechat-jsdk"{
+  let wx: any;
+  export default wx;
+}
